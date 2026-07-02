bf5233a44a7ff5f13ebedadf3e1a2ffeb09aecd4
Ahmed Abdo <ma913222@gmail.com>
2026-07-03 01:16:24 +0300

    feat: complete authentication module

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,25 @@
+import useFavoritesStore from "../store/useFavoritesStore";
+
 function Favorites() {
-  return <h1>Favorites Page</h1>;
+  const favorites = useFavoritesStore(
+    (state) => state.favorites
+  );
+
+  return (
+    <div>
+      <h1>Favorites</h1>
+
+      {favorites.length === 0 ? (
+        <p>No favorite movies yet.</p>
+      ) : (
+        favorites.map((movie) => (
+          <div key={movie.id}>
+            {movie.title}
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
 
 export default Favorites;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,31 @@
+import useAuthStore from "../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
 function Profile() {
-  return <h1>Profile Page</h1>;
+  const user = useAuthStore((state) => state.user);
+  const logout = useAuthStore((state) => state.logout);
+const navigate = useNavigate();
+
+const handleLogout = () => {
+    logout();
+    navigate("/login");
+};
+  return (
+    <div>
+      <h1>Profile</h1>
+
+      <p>
+        <strong>Name:</strong> {user?.name}
+      </p>
+
+      <p>
+        <strong>Email:</strong> {user?.email}
+      </p>
+
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Profile;
